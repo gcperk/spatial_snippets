f4a9525be4d9f401baaea6a8abc8ac5d7f3929de
GPerkins <Genevieve.Perkins@gov.bc.ca>
2019-10-14 17:59:49 -0700

    add shortcut cheat sheet

---
+++ 00.shortcuts.R
@@ -0,0 +1,16 @@
+
+# Shortcuts ---------------------------------------------------------------
+
+Ctrl Enter = To run a line
+
+%>%   ctrl - shift- m 
+<-    alt (-)
+
+
+ctrl I - fix indentation 
+
+# this section ------------------------------------------------------------
+ ctrl -  shift - R
+
+alt shift l =  open the code section 
+alt l       =  close this code section 
